7a0cc7f1ddcc250e0c21e0c0388068b4bfdcd80d
anders kildemand <anders.kildemand@gmail.com>
2026-08-11 14:08:43 +0200

    Might have fixed the black screen on railway

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,1 +1,12 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+declare global {
+  interface Window {
+    VALETUDO_CONFIG?: {
+      apiUrl?: string;
+    };
+  }
+}
+
+export const API_URL =
+  window.VALETUDO_CONFIG?.apiUrl ||
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:8080";
